df1df870beefcc77b559df396d105b190d42b915
Alvaro Salvador Valdivia Calizaya <asvaldivia12@gmail.com>
2019-09-06 11:08:40 -0600

    Creacion proyecto

---
+++ WalletWebApi/WalletWebApi.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WalletWebApi;
+using WalletWebApi.Controllers;
+
+namespace WalletWebApi.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+
+            // Actuar
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
